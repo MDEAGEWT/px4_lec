0eb07df194f068d27c8df508f532e8f9ed8013f0
Kimminkyu <88468818+MDEAGEWT@users.noreply.github.com>
2023-06-08 08:36:17 +0900

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -9,7 +9,7 @@ docker rm $PS_NAME 2>/dev/null
 
 nvidia_smi_result=$(nvidia-smi 2>/dev/null)
 
-if docker images | awk -v image_name="mdeagewt/px4" -v image_tag="lec" '$1 == image_name && $2 == image_tag {found=1; exit} END {exit !found}'; then
+if docker images | awk -v image_name="stmoon/px4" -v image_tag="lec" '$1 == image_name && $2 == image_tag {found=1; exit} END {exit !found}'; then
   if [[ $nvidia_smi_result == *'+'* ]]; then
     echo "run docker image using gpu"
     docker run -it --privileged --gpus all \
@@ -42,5 +42,5 @@ if docker images | awk -v image_name="mdeagewt/px4" -v image_tag="lec" '$1 == im
       --name $PS_NAME mdeagewt/px4:lec bash
   fi
 else
-    echo "download docker image first using \"docker pull mdeagewt/px4_ros2:1.0\""
+    echo "download docker image first using \"docker pull stmoon/px4:1.0\""
 fi
